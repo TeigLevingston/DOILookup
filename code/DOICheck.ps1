035f3dd6c7677474a6d4e98aff51aea6496d97e0
TeigLevingston <74582070+TeigLevingston@users.noreply.github.com>
2025-04-10 22:07:46 -0500

    Update DOICheck.ps1
    Cleaned up some issues and made the font bigger.

--- code/DOICheck.ps1
+++ code/DOICheck.ps1
@@ -3,7 +3,6 @@ Add-Type -AssemblyName System.Windows.Forms
 
 # Import the System.Drawing namespace for colors, sizes, and fonts
 Add-Type -AssemblyName System.Drawing
-
 # Function to validate a DOI URL
 function Test-DOI {
     param (
@@ -15,14 +14,12 @@ function Test-DOI {
         $response = Invoke-WebRequest -Uri $doiUrl -Method Get -Headers $headers -ErrorAction Stop
         write-host "URL: $doiUrl"
         Write-Host "Success: Status Code $($response.StatusCode)"
-        #Write-Host "Response Content: ($response.Content)" -ForegroundColor Green
         $authorstart = $response.Content.IndexOf("author={",[System.StringComparison]::OrdinalIgnoreCase)+8
         $authorend = $response.Content.IndexOf("}",$authorstart )
         $Authors =$response.Content.Substring($authorstart,($authorend-$authorstart)).Trim()
         $titlestart = $response.Content.IndexOf("title={",[System.StringComparison]::OrdinalIgnoreCase)+7
         $titleend = $response.Content.IndexOf("}",$titlestart ) 
         $Title =$response.Content.Substring($titlestart,($titleend-$titlestart)).Trim()
-        #Write-Host $Authors
     } catch {
         Write-Host "Error: $($_.Exception.Message)"
     }
@@ -33,19 +30,19 @@ function Test-DOI {
     }   
 }
 
-
+#function to split the text into lines and extract authors and DOI URLs
 function split-text {
     param (
         [string]$text
     )
-    #write-host $text
+
     $authors=@()
     $doiUrl = @()
     $validURL = @()
     $DOIAuthors = @()
     $DOITitle = @()
 
-    $reflist = $text.split("`n") # | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne "" }
+    $reflist = $text.split("`n") 
     $counter =0
     foreach ($line in $reflist) {
         if ($line.length -eq 0) {
@@ -55,15 +52,15 @@ function split-text {
         write-host $line
             if ($line.Contains(").")) {
                 $authorend = $line.indexof(").")+2
-                #write-host $authorend
+
                 $authors+= $line.Substring(0, $authorend).Trim()
             } else {
                 Write-Host "Warning: Unable to extract author from the line." -ForegroundColor Yellow
                 $authors+= "Author not valid"
             }
-            #write-host $authors[$counter]
+
             $DOIStart = $line.indexOf("https://doi.org/", [System.StringComparison]::OrdinalIgnoreCase)
-            #Write-Host $DOIStart
+
             if ($DOIStart -ne -1) {
                 $doiUrl+= $line.Substring($DOIStart).Trim()
                 $urldata = test-DOI -doiUrl $doiUrl[$counter]
@@ -82,9 +79,6 @@ function split-text {
 
 
 
-
-                write-host $validURL[$counter]
-                #write-host $doiUrl[$counter]
             } else {
                 $doiUrl+= "NO DOI URL"
                 Write-Host "DOI URL not found in the line." -ForegroundColor Yellow
@@ -111,6 +105,7 @@ function split-text {
         Showresultsform -authors $authors -doiUrl $doiUrl -validURL $validURL -DOIAuthors $DOIAuthors -DOITitle $DOITitle -reflist $reflist
 
     }
+    # Function to display the results in a Windows Form
     function Showresultsform {
         param (
             [Array]$authors,
@@ -126,18 +121,18 @@ function split-text {
         $resultsform.Width = $screen.WorkingArea.Width * 0.8  # 80% of screen width
         $resultsform.Height = $screen.WorkingArea.Height * 0.8  # 80% of screen height
         $resultsform.Text = "DOI Validation Results"
-        #$resultsform.Size = New-Object System.Drawing.Size(800, 600)
+        $resultsform.Font = New-Object System.Drawing.Font("Arial", 12)
         $resultsform.StartPosition = "CenterScreen"
 
-        # Create a TextBox to display the results
+
         $textBox = New-Object System.Windows.Forms.RichTextBox
         $textBox.Multiline = $true
         $textBox.Dock = [System.Windows.Forms.DockStyle]::Fill
-        #$textBox.Size = New-Object System.Drawing.Size(800, 600)
-        #$textBox.Location = New-Object System.Drawing.Point(20, 20)
+        $textBox.Font = New-Object System.Drawing.Font("Arial", 12)
+
         $textBox.ReadOnly = $true
         $resultsform.Controls.Add($textBox)
-        # Populate the TextBox with the results
+
         $Backcolorcount=0
         $backcolor=[system.drawing.color]::gray
         for ($i=0; $i -lt $reflist.Count; $i++) {
@@ -147,8 +142,7 @@ function split-text {
                 $backcolor=[system.drawing.color]::Blue
             }
             $backColorcount++
-        
-            #.TrimEnd("`r","`n")
+
                 $textBox.SelectionColor = $backcolor
                 $textBox.AppendText("Reference: " + $reflist[$i] + "`r`n")
                 $textBox.SelectionColor = $backcolor
@@ -165,38 +159,7 @@ function split-text {
                 $textBox.AppendText("Invalid DOI URL: " + $doiUrl[$i] + "`r`n`r`n")
             }
         }
-<##>
-        # Create a DataGridView for displaying tabular data
-        <#
-        $dataGridView = New-Object System.Windows.Forms.DataGridView
-        $dataGridView.Size = New-Object System.Drawing.Size(350, 100)
-        $dataGridView.Location = New-Object System.Drawing.Point(20, 240)
-        $dataGridView.ColumnCount = 5
-        $dataGridView.Columns[0].Name = "APA Authors"
-        $dataGridView.Columns[1].Name = "DOI URL"
-        $dataGridView.Columns[2].Name = "DOI Authors"
-        $dataGridView.Columns[3].Name = "DOI Title"
-        $dataGridView.Columns[4].Name = "Valid DOI URL"
-        $dataGridView.AutoSizeColumnsMode = "Fill"
-        $dataGridView.AllowUserToAddRows = $false
-        $dataGridView.AllowUserToDeleteRows = $false
-        for ($i=0; $i -lt $authors.Count; $i++) {
 
-            $dataGridView.Rows.Add($authors[$i],$doiUrl[$i],$DOIAuthors[$i],$DOITitle[$i],$doiUrl[$i]) 
-            <#
-            $textBox.AppendText("Authors: " + + "`r`n")
-            $textBox.AppendText("Valid DOI URL: " +  + "`r`n")
-            $textBox.AppendText("DOI Authors: " +  + "`r`n")
-            $textBox.AppendText("DOI Title: " +  + "`r`n`r`n")
-        if ($validURL[$i]) {
-            $textBox.AppendText("Valid DOI URL: " +  + "`r`n")
-        } else {$textBox.AppendText("Invalid DOI URL: " + $doiUrl[$i] + "`r`n")
-            $textBox.AppendText("Invalid DOI URL: " + $doiUrl[$i] + "`r`n")
-        }
-    }
-
-        $dataGridView.Rows.Add("Row 2, Col 1", "Row 2, Col 2")
-        #>
         
         $resultsform.Controls.Add($dataGridView)
         #>
@@ -205,52 +168,47 @@ function split-text {
         [void]$resultsform.ShowDialog()
     }
 
-# Add a Windows Form for DOI input and validation
+# Add a Windows Form for DOI input
 Add-Type -AssemblyName System.Windows.Forms
 
-# Create the form
+
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "DOI Validator"
 $screen = [System.Windows.Forms.Screen]::PrimaryScreen
 $form.Width = $screen.WorkingArea.Width * 0.8  # 80% of screen width
 $form.Height = $screen.WorkingArea.Height * 0.8  # 80% of screen height
-#form.Size = New-Object System.Drawing.Size(400, 300)
+$form.Font = New-Object System.Drawing.Font("Arial", 12)
 $form.StartPosition = "CenterScreen"
-
-# Create a multiline TextBox for input
 $textBox = New-Object System.Windows.Forms.TextBox
 $textBox.Multiline = $true
-$boxheight = $form.ClientSize.Height - 300
-$textBox.Size = New-Object System.Drawing.Size($form.ClientSize.Width, $boxheight)
+$textBox.Size = New-Object System.Drawing.Size($form.Width - 40, $form.Height - 350) 
 $textBox.Multiline = $true
 $textBox.Location = New-Object System.Drawing.Point(20, 20)
 $textBox.ScrollBars = "Vertical"    
-# The size will be automatically adjusted by the Dock property
-#$textBox.Dock = [System.Windows.Forms.DockStyle]::Fill
+$textBox.Font = New-Object System.Drawing.Font("Arial", 12)
+$textBox.Dock = [System.Windows.Forms.DockStyle]::Fill
 $form.Controls.Add($textBox)
 
 $button = New-Object System.Windows.Forms.Button
 $button.Text = "Validate"
 $button.Dock = [System.Windows.Forms.DockStyle]::Bottom
-#$button.Dock = [System.Windows.Forms.DockStyle]::Right
-
-$button.Size = New-Object System.Drawing.Size(100, 30)
+$button.Size = New-Object System.Drawing.Size(150, 40)  
 $button.Location = New-Object System.Drawing.Point(140, 200)
+$button.Font = New-Object System.Drawing.Font("Arial", 12)
 $form.Controls.Add($button)
 
-# Create a CheckBox for additional options
+
+<#This is for a potential featuer later
 $checkBox = New-Object System.Windows.Forms.CheckBox
 $checkBox.Text = "Check All URLs"
-$checkBox.Size = New-Object System.Drawing.Size(100, 20)
+$checkBox.Size = New-Object System.Drawing.Size(150, 30)  
 $checkBox.Dock = [System.Windows.Forms.DockStyle]::Bottom
-#$checkBox.Dock = [System.Windows.Forms.DockStyle]::Left
-#$checkBox.Location = New-Object System.Drawing.Point(20, 180)
+$checkBox.Font = New-Object System.Drawing.Font("Arial", 12)
 $form.Controls.Add($checkBox)
-
+#>
 # Create a Button for validation
 
 
-# Add event handler for the button click
 $button.Add_Click({
     $incomingText = $textBox.Text.Trim()
     split-text -text $incomingText
@@ -259,4 +217,4 @@ $button.Add_Click({
 
 
 
-# Show the form
+
